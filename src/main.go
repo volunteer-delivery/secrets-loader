5b2043c5bf7dc654a87989e48066f66114f06a7a
TArch64 <tarasturchenko1551@gmail.com>
2022-11-14 23:46:56 +0200

    test

--- src/main.go
+++ src/main.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"secrets-loader/src/loader"
-	"secrets-loader/src/logger"
+//	"secrets-loader/src/loader"
+//	"secrets-loader/src/logger"
 
 	. "github.com/urfave/cli/v2"
 )
 
-func initLoaderConfig(ctx *Context) *loader.Config {
-	config := loader.NewConfig()
-	config.Path = ctx.String("path")
-	config.Region = ctx.String("region")
-	config.Label = ctx.String("label")
-	return config
-}
-
-func initLogger(ctx *Context) *logger.Logger {
-	level, _ := logger.ParseLogLevel(ctx.String("log-level"))
-	return logger.NewLogger(level)
-}
+//func initLoaderConfig(ctx *Context) *loader.Config {
+//	config := loader.NewConfig()
+//	config.Path = ctx.String("path")
+//	config.Region = ctx.String("region")
+//	config.Label = ctx.String("label")
+//	return config
+//}
+//
+//func initLogger(ctx *Context) *logger.Logger {
+//	level, _ := logger.ParseLogLevel(ctx.String("log-level"))
+//	return logger.NewLogger(level)
+//}
 
 func main() {
 	app := &App{
@@ -49,12 +49,14 @@ func main() {
                     },
 				},
                 Action: func(ctx *Context) error {
-                    loggerInstance := initLogger(ctx)
-                    loaderInstance := loader.NewLoader(loggerInstance, initLoaderConfig(ctx))
+//                    loggerInstance := initLogger(ctx)
+//                    loaderInstance := loader.NewLoader(loggerInstance, initLoaderConfig(ctx))
+//
+//                    for _, secret := range loaderInstance.Load() {
+//                        loggerInstance.Data("%v=%v", secret.Name, secret.Value)
+//                    }
 
-                    for _, secret := range loaderInstance.Load() {
-                        loggerInstance.Data("%v=%v", secret.Name, secret.Value)
-                    }
+                    fmt.Println("Hello from cli")
 
                     return nil
                 },
@@ -64,14 +66,14 @@ func main() {
 			&StringFlag{
 				Name:  "log-level",
 				Value: "none",
-				Action: func(ctx *Context, level string) error {
-					_, ok := logger.ParseLogLevel(ctx.String("log-level"))
-					if !ok {
-						return fmt.Errorf("invalid log level %v", level)
-					}
-					return nil
-				},
-				Usage: fmt.Sprintf("Set logging `level`. Available levels: %v", logger.LogLevelNames),
+//				Action: func(ctx *Context, level string) error {
+//					_, ok := logger.ParseLogLevel(ctx.String("log-level"))
+//					if !ok {
+//						return fmt.Errorf("invalid log level %v", level)
+//					}
+//					return nil
+//				},
+//				Usage: fmt.Sprintf("Set logging `level`. Available levels: %v", logger.LogLevelNames),
 			},
 		},
 	}
